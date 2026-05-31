db080be54fa82101c6ecd4458bd64e57a41a47e7
Abdelaziz Mohamed Abdelaziz <abdelazizmohameddewedar@gmail.com>
2026-05-31 23:30:18 +0300

    Refactor EditProfileScreen to    improve UI with Herafi theme components

--- lib/features/profile/presention/screen/edit_profile_screen.dart
+++ lib/features/profile/presention/screen/edit_profile_screen.dart
@@ -1,185 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:herafi_app/app/theme/herafi_colors.dart';
+import 'package:herafi_app/app/theme/herafi_style.dart';
+import 'package:herafi_app/core/constant/assets/herafi_image.dart';
+import 'package:herafi_app/core/widget/custom_text_form.dart';
+import 'package:herafi_app/core/widget/cutom_button.dart';
 
-class EditProfileScreen extends StatefulWidget {
-  const EditProfileScreen({super.key});
-
-  @override
-  State<EditProfileScreen> createState() => _EditProfileScreenState();
-}
-
-class _EditProfileScreenState extends State<EditProfileScreen> {
-  final nameController = TextEditingController(text: 'عبدالعزيز محمد');
-
-  final phoneController = TextEditingController(text: '01012345678');
-
-  final jobController = TextEditingController(text: 'مطور تطبيقات');
-
-  final locationController = TextEditingController(text: 'بنها - القليوبية');
+class EditProfileScreen extends StatelessWidget {
+  const EditProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffF5F5F5),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 220,
-            pinned: true,
-            backgroundColor: const Color(0xff11122F),
-            leading: IconButton(
-              icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              title: const Text(
-                'تعديل الملف الشخصي',
-                style: TextStyle(color: Colors.white),
-              ),
-              background: Container(
-                decoration: const BoxDecoration(
-                  color: Color(0xff11122F),
-                  borderRadius: BorderRadius.vertical(
-                    bottom: Radius.circular(35),
-                  ),
-                ),
-                child: Center(
-                  child: Stack(
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      const CircleAvatar(
-                        radius: 55,
-                        backgroundImage: AssetImage('assets/profile.jpg'),
-                      ),
-
-                      GestureDetector(
-                        onTap: () {
-                          // pick image
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            color: const Color(0xffF2A51A),
-                            shape: BoxShape.circle,
-                            border: Border.all(color: Colors.white, width: 2),
-                          ),
-                          child: const Icon(
-                            Icons.camera_alt,
-                            size: 18,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+      backgroundColor: HerafiColors.background,
+      appBar: AppBar(
+        title: Text('تعديل الملف الشخصي', style: HerafiStyles.text26boldWhite),
+        centerTitle: true,
+        backgroundColor: HerafiColors.darkBlueColor,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 250,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: HerafiColors.darkBlueColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25),
                 ),
               ),
-            ),
-          ),
-
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
+              child: Stack(
+                alignment: Alignment.bottomRight,
                 children: [
-                  _customField(
-                    controller: nameController,
-                    label: 'الاسم',
-                    icon: Icons.person_outline,
-                  ),
-
-                  const SizedBox(height: 18),
-
-                  _customField(
-                    controller: phoneController,
-                    label: 'رقم الهاتف',
-                    icon: Icons.phone_outlined,
-                    keyboardType: TextInputType.phone,
-                  ),
-
-                  const SizedBox(height: 18),
-
-                  _customField(
-                    controller: jobController,
-                    label: 'المهنة',
-                    icon: Icons.work_outline,
-                  ),
-
-                  const SizedBox(height: 18),
-
-                  _customField(
-                    controller: locationController,
-                    label: 'الموقع',
-                    icon: Icons.location_on_outlined,
-                  ),
-
-                  const SizedBox(height: 35),
-
-                  SizedBox(
-                    width: double.infinity,
-                    height: 58,
-                    child: ElevatedButton.icon(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xff11122F),
-                        elevation: 5,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
-                        ),
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 100,
+                    child: ClipOval(
+                      child: Image.asset(
+                        HerafiImage.user,
+                        width: 150,
+                        height: 200,
+                        fit: BoxFit.contain,
                       ),
-                      onPressed: () {
-                        // update profile
-                      },
-                      icon: const Icon(
-                        Icons.save_outlined,
-                        color: Colors.white,
-                      ),
-                      label: const Text(
-                        'حفظ التعديلات',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
+                    ),
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.camera_alt),
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _customField({
-    required TextEditingController controller,
-    required String label,
-    required IconData icon,
-    TextInputType? keyboardType,
-  }) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 10,
-            offset: const Offset(0, 4),
-            color: Colors.black.withOpacity(.05),
-          ),
-        ],
-      ),
-      child: TextField(
-        controller: controller,
-        keyboardType: keyboardType,
-        textAlign: TextAlign.right,
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          contentPadding: const EdgeInsets.symmetric(
-            horizontal: 18,
-            vertical: 18,
-          ),
-          hintText: label,
-          prefixIcon: Icon(icon, color: const Color(0xffF2A51A)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                children: [
+                  SizedBox(height: 16),
+                  CustomTextForm(
+                    text: 'تعديل الاسم',
+                    kType: TextInputType.text,
+                    pIcon: Icons.person,
+                  ),
+                  SizedBox(height: 16),
+                  CustomTextForm(
+                    text: 'تعديل الرقم',
+                    kType: TextInputType.text,
+                    pIcon: Icons.phone,
+                  ),
+                  SizedBox(height: 16),
+                  CustomTextForm(
+                    text: 'تعديل الحرفة',
+                    kType: TextInputType.text,
+                    pIcon: Icons.handyman,
+                  ),
+                  SizedBox(height: 16),
+                  CustomTextForm(
+                    text: 'تعديل الموقع',
+                    kType: TextInputType.text,
+                    pIcon: Icons.location_on,
+                  ),
+                  SizedBox(height: 16),
+                  CustomButton(
+                    onPressed: () {},
+                    text: "حفظ التعديلات",
+                    color: HerafiColors.goldColor,
+                    textStyle: HerafiStyles.text20White,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
